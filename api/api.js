6a3058efd09d26bad02fb88f03e9fbba899aed3e
Joey R <rehmanjoey@gmail.com>
2022-02-02 20:32:24 +0000

    GET input site

--- api/api.js
+++ api/api.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
+const path = require('path');
+const { readFile } = require('fs').promises;
 const { 
     getStatement,
     getPossibleUsers
 } = require('../src/builder/builder.js');
+
+router.get('/', async (req, res, next) => {
+    res.send(await readFile(path.join('./index.html'), 'utf-8'));
+});
 
 router.post('/', async (req, res, next) => {
     const sqlStatement = getStatement(req.body);
