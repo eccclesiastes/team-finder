c4155d669edc96dfaf908c484817761d959849b1
Joey R <rehmanjoey@gmail.com>
2022-02-08 14:18:17 +0000

    Removed awaits, added POST /update, sending stringified result on create

--- api/api.js
+++ api/api.js
@@ -4,17 +4,15 @@ const { readFile } = require('fs').promises;
 const { 
     getStatement,
     getPossibleUsers,
-    createUser
+    createUser,
+    getUpdateStatement,
+    updateUser
 } = require('../src/builder/builder.js');
 
 router.get('/', async (req, res, next) => {
-    res.send(await readFile(path.join('./index.html'), 'utf-8'));
-});
-
-router.post('/', async (req, res, next) => {
     const sqlStatement = getStatement(req.body);
 
-    const jsonQuery = await getPossibleUsers(sqlStatement, (result) => {
+    const jsonQuery = getPossibleUsers(sqlStatement, (result) => {
         res.send(JSON.stringify(result));
     });
 });
@@ -22,11 +20,27 @@ router.post('/', async (req, res, next) => {
 router.post('/create', async (req, res, next) => {
     createUser(req.body, (result) => {
          if (result) {
-            res.status(201).send();
+            res.status(201).send(JSON.stringify(result));
         } else {
             res.status(400).send();
         };
     });
+});
+
+router.post('/update', async (req, res, next) => {
+    const sqlStatement = getUpdateStatement(req.body);
+    
+    if (sqlStatement === 1) {
+        res.status(400).send();
+    } else {
+        updateUser(sqlStatement, (result) => {
+            if (result.affectedRows > 0) {
+                res.send(JSON.stringify(result));
+            } else {
+                res.status(400).send();
+            }
+        });
+    };
 });
 
 module.exports = { router };
